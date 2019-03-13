a9f2d0a50cd06d4de1dc6919b674d872cc601d47
DinhTrinh <DinhTrinhIT@users.noreply.github.com>
2019-03-13 21:30:24 +0700

    update

---
+++ WebTinTuc/WebTinTuc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTinTuc.Models;
+
+namespace WebTinTuc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        { 
+        
+            ViewBag.Hello="Hello Word";
+            ViewData["XinChao"] = "Xin Chào";
+            
+            var model = new HelloModel();
+            model.FirstName = "Trịnh";
+            model.LastName = "Nguyễn Đình";
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
